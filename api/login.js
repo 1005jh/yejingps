20f082b8052aefef5f44c7eae3f984bd6ac7218f
1005jh <kjhhy1005@gmail.com>
2023-07-14 23:58:11 +0900

    1

--- api/login.js
+++
@@ -1,6 +0,0 @@
-const response = await axios.post('https://mo-inda.shop/users/login', option);
-
-export const login = async (payload) => {
-  const data = await instance.post('users/login', payload);
-  return data;
-};
