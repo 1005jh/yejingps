127df30bdb209263f56786d7ddaabc795bc64f58
1005jh <kjhhy1005@gmail.com>
2023-08-21 22:14:25 +0900

    웹소켓 쿠키 토큰 검증 추가

--- src/auth/jwt/jwt.guard.ts
+++ src/auth/jwt/jwt.guard.ts
@@ -1,9 +1,72 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+// import { ExecutionContext, Injectable } from '@nestjs/common';
+// import { AuthGuard as NestAuthGuard } from '@nestjs/passport';
+
+// @Injectable()
+// export class AuthGuard extends NestAuthGuard('jwt') {
+//   canActivate(context: ExecutionContext): any {
+//     return super.canActivate(context);
+//   }
+// }
+// jwt.guard.ts
+
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard as NestAuthGuard } from '@nestjs/passport';
+import * as cookie from 'cookie'; // cookie-parser 패키지를 사용
+import * as jwt from 'jsonwebtoken'; // jwt를 import합니다.
+import { AuthService } from '../auth.service'; // AuthService를 import합니다.
+import { Socket } from 'socket.io';
+
+interface Payload {
+  username?: string;
+  nickname?: string;
+  id: number;
+}
 
 @Injectable()
 export class AuthGuard extends NestAuthGuard('jwt') {
-  canActivate(context: ExecutionContext): any {
-    return super.canActivate(context);
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isWsContext = context.getType() === 'ws';
+
+    if (isWsContext) {
+      const client: Socket = context.switchToWs().getClient();
+      const cookies = client.handshake.headers.cookie;
+      const parsedCookies = cookie.parse(cookies || '');
+      const token = parsedCookies.jwt; // 'jwt'는 쿠키의 키입니다.
+
+      // 토큰을 검사하는 로직
+      if (!(await this.validateToken(token))) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      return true;
+    } else {
+      super.canActivate(context);
+      return true;
+    }
+  }
+
+  async validateToken(token: string): Promise<boolean> {
+    try {
+      const decoded: Payload = jwt.verify(
+        token,
+        process.env.SECRETKEY,
+      ) as Payload;
+      const user = await this.authService.validateUser(decoded);
+
+      if (!user) {
+        return false;
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
