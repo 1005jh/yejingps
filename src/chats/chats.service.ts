6c9a09a4511722bfc4d182eee24710a4e2ce3773
1005jh <kjhhy1005@gmail.com>
2023-08-22 21:27:11 +0900

    1

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -20,7 +20,7 @@ export class ChatsService {
 
   async joinUesr(user: UserEntity, roomId: string) {
     try {
-      console.log(roomId, '서비스에서 룸아이디 확인해보자');
+      console.log(user, '서비스에서 유저 확인해보자!!');
       const room = await this.roomRepository.findOne({
         where: { roomId: roomId },
       });
